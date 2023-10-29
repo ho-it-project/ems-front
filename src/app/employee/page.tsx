9c04182224c10596bfe6990e16d9aa8bcd88be61
Denovo <vo0v0000@naver.com>
2023-10-30 05:02:36 +0900

    refactor: add EmployeePrototypes

--- src/app/employee/page.tsx
+++ src/app/employee/page.tsx
@@ -1,11 +1,11 @@
-import { EmployeeContainer } from "@/components/container/EmployeeContainer";
 import { TabWrapper } from "@/components/layout/TabWrapper";
+import { EmployeePrototype } from "@/components/prototypes/Employee";
 
 export default function Home() {
   return (
     <TabWrapper
       contents={[
-        { title: "직원관리", content: <EmployeeContainer /> },
+        { title: "직원관리", content: <EmployeePrototype /> },
         {
           title: "test",
           content: <div />,
