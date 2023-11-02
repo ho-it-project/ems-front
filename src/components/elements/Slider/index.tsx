5f3cfd5f635179f5df67d08eba0970da065236e3
Denovo <vo0v0000@naver.com>
2023-11-03 02:33:51 +0900

    feat: shadcn Slider -> Slider 직접개발: mark이슈

--- src/components/elements/Slider/index.tsx
+++ src/components/elements/Slider/index.tsx
@@ -1,64 +1,106 @@
 "use client";
-import { Slider } from "@/components/ui/slider";
 import { cn } from "@/lib/utils";
 import { useState } from "react";
 
-type SliderProps = React.ComponentProps<typeof Slider>;
+interface SliderProps {
+  className?: string;
+  defaultValue?: number;
+  max?: number;
+  step?: number;
+}
 
 export function SliderDemo({
-  className,
-  defaultValue,
+  defaultValue = 3,
   max = 10,
-  ...props
+  step = 1,
 }: SliderProps) {
-  const [value, setValue] = useState(defaultValue || [0]);
+  const [value, setValue] = useState(defaultValue);
   const markArray = Array.from(Array(max + 1).keys());
-  const onChange = (value: number[]) => {
-    setValue(value);
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(Number(e.target.value));
   };
   console.log(value);
   return (
     <div className="relative flex w-full flex-col gap-[0.8rem]">
       <div className="relative flex w-full ">
         <div
-          className="itmes-center absolute flex h-[1rem] justify-between px-[1.2rem]"
+          className="itmes-center absolute flex h-[1rem] justify-between "
           style={{ width: "calc(100% - 1rem)" }}
         >
+          <div className="flex w-full translate-x-[0.5rem] items-center">
+            {markArray.slice(0, max).map((mark, index) => {
+              return (
+                <span
+                  key={index}
+                  className={cn(
+                    "h-1 w-full",
+                    index < value ? "bg-main" : "bg-lgrey"
+                  )}
+                />
+              );
+            })}
+          </div>
           {markArray.map((_, index) =>
             index % 5 === 0 ? (
-              <span
-                className={cn(
-                  " absolute top-[0.05rem] block h-[1rem] w-[1rem] -translate-y-1/2 rounded-full",
-                  index <= value[0] ? "bg-main" : "bg-lgrey"
-                )}
+              <div
+                className={cn("absolute flex h-[1rem] w-[1rem] justify-center")}
                 style={{ left: `${(100 / max) * index}%` }}
                 key={index}
-              />
+              >
+                <span
+                  className={cn(
+                    "h-full w-full rounded-full ",
+                    index <= value ? "bg-main" : "bg-lgrey"
+                  )}
+                />
+              </div>
             ) : (
-              <span
-                className={cn(
-                  "absolute  top-[0.05rem] block h-[0.9rem] w-[0.1rem] -translate-y-1/2 ",
-                  index <= value[0] ? "bg-main" : "bg-lgrey"
-                )}
+              <div
+                className={cn("absolute flex h-[1rem] w-[1rem] justify-center")}
                 style={{ left: `${(100 / max) * index}%` }}
                 key={index}
-              />
+              >
+                <span
+                  className={cn(
+                    "absolute block h-[0.9rem] w-[0.1rem] ",
+                    index <= value ? "bg-main" : "bg-lgrey"
+                  )}
+                  key={index}
+                />
+              </div>
             )
-          )}
+          )}{" "}
         </div>
-
-        <Slider
-          defaultValue={value}
-          max={max}
-          step={1}
-          className={cn(className)}
-          {...props}
-          onValueChange={onChange}
-        />
       </div>
+      <span
+        className="absolute  -left-[0.7rem] -top-[0.7rem] h-[2.4rem] w-[2.4rem] rounded-full bg-white opacity-100"
+        style={{
+          zIndex: 1,
+          left: `calc(${(100 / max) * value}% - ${0.7 + (1 / max) * value}rem)`,
+        }}
+      />
+      <span
+        className="fontSize-regular  absolute -left-[0.7rem] -top-[0.7rem] mt-[2.4rem] h-[2.4rem]  w-[2.4rem] text-center"
+        style={{
+          zIndex: 1,
+          left: `calc(${(100 / max) * value}% - ${0.7 + (1 / max) * value}rem)`,
+        }}
+      >
+        {value}
+      </span>
+      <input
+        type="range"
+        min="0"
+        max={max}
+        step={step}
+        value={value}
+        onChange={onChange}
+        className="absolute  -top-[0.5rem] h-[2.4rem] w-full cursor-pointer appearance-none rounded-full bg-white opacity-0 "
+        style={{ zIndex: 2 }}
+      />
       <div className="fontSize-small flex w-full justify-between text-center text-lgrey">
         {markArray.map((mark, index) =>
-          index % 5 === 0 && value[0] !== index ? (
+          index % 5 === 0 && value !== index ? (
             <span
               className={cn("block h-[1rem] w-[1rem] rounded-full")}
               key={index}
@@ -70,46 +112,6 @@ export function SliderDemo({
           )
         )}
       </div>
-      <div className="fontSize-small fontSize-regular absolute top-[1.6rem] flex w-full justify-between text-center">
-        {markArray.map((mark, index) =>
-          value[0] === index ? (
-            <span
-              className={cn("block h-[1rem] w-[2.4rem] rounded-full")}
-              key={index}
-            >
-              {value}
-            </span>
-          ) : (
-            <span className={cn("block h-[0.9rem] w-[1rem]")} key={index} />
-          )
-        )}
-      </div>
     </div>
   );
-}
-
-{
-  /* <div className="flex w-full justify-between">
-{markArray.map((mark, index) =>
-  index % 5 === 0 ? (
-    <span
-      className={cn(
-        "absolute block h-[1rem] w-[1rem] -translate-y-1/2 rounded-full",
-        index <= value ? "bg-main" : "bg-lgrey"
-      )}
-      style={{ left: `${(100 / max) * index}%` }}
-      key={index}
-    />
-  ) : (
-    <span
-      className={cn(
-        "absolute block h-[1rem] w-[0.1rem] -translate-y-1/2",
-        index <= value ? "bg-main" : "bg-lgrey"
-      )}
-      style={{ left: `${(100 / max) * index}%` }}
-      key={index}
-    />
-  )
-)}
-</div> */
 }
