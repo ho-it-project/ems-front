6225f73d1b6c349b26f0448dada0ba542e40e9db
Jo Young Jae <yj.anthonyjo@gmail.com>
2023-11-06 15:01:35 +0900

    chore: add env and validation

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -2,12 +2,18 @@ import Joi from "joi";
 
 const schema = Joi.object({
   NEXT_PUBLIC_NODE_ENV: Joi.string().valid("dev", "prod"),
+  NEXT_PUBLIC_API_PREFIX: Joi.string(),
+  NEXT_PUBLIC_PROTOCOL: Joi.string().valid("http", "https"),
+  NEXT_PUBLIC_ROOT_DOMAIN: Joi.string(),
 });
 
 export const env = {
   NEXT_PUBLIC_NODE_ENV: (process.env.NEXT_PUBLIC_NODE_ENV || "prod") as
     | "dev"
     | "prod",
+  NEXT_PUBLIC_API_PREFIX: process.env.NEXT_PUBLIC_API_PREFIX,
+  NEXT_PUBLIC_PROTOCOL: process.env.NEXT_PUBLIC_PROTOCOL || "https",
+  NEXT_PUBLIC_ROOT_DOMAIN: process.env.NEXT_PUBLIC_ROOT_DOMAIN,
 };
 
 export function validateEnv() {
