005bb3f594c822326b8ac017f884bbb04df38c62
Denovo <vo0v0000@naver.com>
2023-11-08 04:01:41 +0900

    feat: request 페이지 상태 ui

--- src/components/container/Request/index.tsx
+++ src/components/container/Request/index.tsx
@@ -1,3 +1,4 @@
+import { Tag } from "@/components/elements/Tag";
 import { ProgressTracker } from "@/components/module/common/ProgressTracker";
 function coordinateOnCircle(
   radius: number,
@@ -16,14 +17,14 @@ function coordinateOnCircle(
 /**
  * TODO: RequestContainer
  * 요청값에대한 위도경도 값이 들어오면 해당위치에 점을 찍어주는 컴포넌트
- * 
+ *
  * 공식 정의해야함
  * 각요청은 5분간격으로 10km씩 요청함
- * 
- * 
+ *
+ *
  * 11월 8일 생각
  * 각 요청의 평균 거리를 구해서 원의 반지름을 구함
- * 
+ *
  *
  */
 export const RequestContainer = () => {
@@ -35,7 +36,41 @@ export const RequestContainer = () => {
     <>
       <div className=" h-full w-full">
         <div className="flex h-full w-full flex-col gap-[3rem]">
-          <div className=" relative flex h-full flex-col items-center gap-[2rem] px-[4.4rem]">
+          <div className="relative flex h-full flex-col items-center gap-[2rem] px-[4.4rem]">
+            <div className="fixed right-[2.4rem] z-50 flex flex-col gap-[0.8rem]">
+              <div className="fontSize-regular -gap-[0.5rem] flex h-[5rem] w-[5rem] flex-col items-center justify-center rounded-lg border-[0.2rem] border-main text-center">
+                <span className="h-[1.6rem]">요청</span>
+                <span>10</span>
+              </div>
+              <div className="fontSize-regular -gap-[0.5rem] flex h-[5rem] w-[5rem] flex-col items-center justify-center rounded-lg border-[0.2rem] border-main text-center">
+                <span className="h-[1.6rem]">요청</span>
+                <span>10</span>
+              </div>
+              <div className="fontSize-regular -gap-[0.5rem] flex h-[5rem] w-[5rem] flex-col items-center justify-center rounded-lg border-[0.2rem] border-main text-center">
+                <span className="h-[1.6rem]">요청</span>
+                <span>10</span>
+              </div>
+              <div className="fontSize-regular -gap-[0.5rem] flex h-[5rem] w-[5rem] flex-col items-center justify-center rounded-lg border-[0.2rem] border-main text-center">
+                <span className="h-[1.6rem]">요청</span>
+                <span>10</span>
+              </div>
+              <div>
+                <Tag
+                  text="요청중"
+                  className="w-[5rem]"
+                  bgColor="main"
+                  border="none"
+                  color="white"
+                />
+                <Tag
+                  text="6:11"
+                  className="w-[5rem]"
+                  bgColor="transparent"
+                  color="grey"
+                  border="none"
+                />
+              </div>
+            </div>
             <div className="absolute top-[50%] z-[1] h-[12rem] w-[12rem] -translate-y-[50%] rounded-full bg-main"></div>
             <div className="absolute top-[50%] z-[1] h-[16.4rem] w-[16.4rem] -translate-y-[50%] rounded-full  border-[0.2rem] bg-transparent">
               {Array.from({ length: numberOfDots }, (_, index) => {
