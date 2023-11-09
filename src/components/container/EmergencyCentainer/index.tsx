02d9b2d2cf6c4d86e79a189fdc872609f2dd46e3
Denovo <vo0v0000@naver.com>
2023-11-10 08:00:14 +0900

    feat: emergency center page 무한스크롤 뼈대

--- src/components/container/EmergencyCentainer/index.tsx
+++ src/components/container/EmergencyCentainer/index.tsx
@@ -1,53 +1,75 @@
+"use client";
+
 import { EmergencyCenterList } from "@/components/module/EmergencyCenter/EmergencyCenterList";
 import { EmergencyCenterPageHeader } from "@/components/module/EmergencyCenter/EmergencyCenterPageHeader";
+import { useEffect, useRef, useState } from "react";
 
 export const EmergencyCenterContainer = () => {
+  const emergencyCenterListRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    console.log(emergencyCenterListRef?.current?.scrollHeight);
+  }, []);
+  const [emergecyCenters, setEmergencyCenters] = useState([
+    {
+      emergency_center_name: "test",
+      emergency_center_type: "test",
+      distance: "test",
+      emergency_center_address: "test",
+      phone_number: "test",
+    },
+    {
+      emergency_center_name: "test",
+      emergency_center_type: "test",
+      distance: "test",
+      emergency_center_address: "test",
+      phone_number: "test",
+    },
+    {
+      emergency_center_name: "test",
+      emergency_center_type: "test",
+      distance: "test",
+      emergency_center_address: "test",
+      phone_number: "test",
+    },
+    {
+      emergency_center_name: "test",
+      emergency_center_type: "test",
+      distance: "test",
+      emergency_center_address: "test",
+      phone_number: "test",
+    },
+    {
+      emergency_center_name: "test",
+      emergency_center_type: "test",
+      distance: "test",
+      emergency_center_address: "test",
+      phone_number: "test",
+    },
+  ]);
+
   return (
     <div className=" h-full w-full px-[2rem] py-[1.6rem]">
       <div className="flex h-full w-full flex-col overflow-hidden">
         <div className="mb-[1rem]  pr-[4rem]">
           <EmergencyCenterPageHeader />
         </div>
-        <div className="h-full w-full overflow-scroll pr-[4rem]">
-          <EmergencyCenterList
-            emergency_center_list={[
-              {
-                emergency_center_name: "test",
-                emergency_center_type: "test",
-                distance: "test",
-                emergency_center_address: "test",
-                phone_number: "test",
-              },
-              {
-                emergency_center_name: "test",
-                emergency_center_type: "test",
-                distance: "test",
-                emergency_center_address: "test",
-                phone_number: "test",
-              },
-              {
-                emergency_center_name: "test",
-                emergency_center_type: "test",
-                distance: "test",
-                emergency_center_address: "test",
-                phone_number: "test",
-              },
-              {
-                emergency_center_name: "test",
-                emergency_center_type: "test",
-                distance: "test",
-                emergency_center_address: "test",
-                phone_number: "test",
-              },
-              {
-                emergency_center_name: "test",
-                emergency_center_type: "test",
-                distance: "test",
-                emergency_center_address: "test",
-                phone_number: "test",
-              },
-            ]}
-          />
+        <div
+          className="h-full w-full overflow-scroll pr-[4rem]"
+          ref={emergencyCenterListRef}
+          onScroll={(e) => {
+            if (
+              e.currentTarget?.scrollHeight - e.currentTarget?.scrollTop ===
+              e.currentTarget?.clientHeight
+            ) {
+              console.log("fetch more");
+              setEmergencyCenters((prev) => [
+                ...prev,
+                ...new Array(5).fill(prev[0]),
+              ]);
+            }
+          }}
+        >
+          <EmergencyCenterList emergency_center_list={emergecyCenters} />
         </div>
       </div>
     </div>
