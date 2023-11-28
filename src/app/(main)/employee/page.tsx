3ff800144e14d069936efe4d8863c6624cdf9f40
Jo Young Jae <yj.anthonyjo@gmail.com>
2023-11-28 16:38:14 +0900

    feat: employee page, api, popup 수정

--- src/app/(main)/employee/page.tsx
+++ src/app/(main)/employee/page.tsx
@@ -1,5 +1,10 @@
 import { TabWrapper } from "@/components/layout/TabWrapper";
 import { EmployeePrototype } from "@/components/prototypes/Employee";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "직원관리",
+};
 
 export default function Home() {
   return (
--- src/components/module/Employee/EmployeePageHeader.tsx
+++ src/components/module/Employee/EmployeePageHeader.tsx
@@ -1,28 +1,68 @@
 import { Input } from "@/components/elements/Input";
 import { PageHeader } from "@/components/elements/PageHeader";
+import { useToast } from "@/components/ui/use-toast";
+import { usePostApi } from "@/hooks/api";
+import { EmployeeAdd, employeeRoles } from "@/types/model/employee";
+import Joi from "joi";
 import Image from "next/image";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useMemo, useState } from "react";
 import { EmployeeInfoPopUpButton } from "./EmployeeInfoPopUpButton";
 const searchTypes = ["이름", "ID"] as const;
 
 export const EmployeePageHeader = ({
   onSearch,
+  refetch,
 }: {
   onSearch: (
     searchType: (typeof searchTypes)[number],
     value: string
   ) => () => void;
+  refetch: () => unknown;
 }) => {
   const [searchType, setSearchType] =
     useState<(typeof searchTypes)[number]>("ID");
+
   const [value, setValue] = useState<string>("");
+  const router = useRouter();
+
+  const { mutate: mutateOnAdd } = usePostApi("/ems/employees", {
+    useLoader: true,
+  });
+  const { toast } = useToast();
+
+  const schema = useMemo(() => {
+    return Joi.object({
+      id_card: Joi.string().min(1),
+      password: Joi.string().min(1),
+      employee_name: Joi.string().min(1),
+      role: Joi.string().valid(...employeeRoles),
+    });
+  }, []);
+
+  const onSubmit = async (employeeInfo: EmployeeAdd) => {
+    const { error } = schema.validate(employeeInfo);
+    if (error) {
+      // console.error(error);
+      toast({ description: error.message });
+      return false;
+    } else {
+      await mutateOnAdd({ body: { employees: [employeeInfo] } });
+      refetch();
+      return true;
+    }
+  };
+
   return (
     <PageHeader
       title="회사 정보"
       fontSize="small-l"
       color="grey"
       button={
-        <button className="h-fit rounded-full text-[0.8rem] text-white">
+        <button
+          className="h-fit rounded-full text-[0.8rem] text-white"
+          onClick={() => router.push("/")}
+        >
           <Image
             src="/icon/back-arrow.svg"
             width={24}
@@ -33,7 +73,7 @@ export const EmployeePageHeader = ({
       }
     >
       <div className="flex gap-[1rem]">
-        <div className="fontSize-small rounded-lg border-[0.2rem] border-main px-[1.2rem] py-[0.8rem] text-main">
+        <div className="fontSize-small rounded-lg border-[0.2rem] border-main px-[1.2rem] text-main">
           <div className="flex items-center gap-[0.6rem]">
             {/* 검색 */}
             <select
@@ -59,7 +99,6 @@ export const EmployeePageHeader = ({
               onChange={(e) => setValue(e.currentTarget.value)}
               border="none"
               width="w-[80%]"
-              inputBoxClassName="h-[24px]"
             />
             <button onClick={onSearch(searchType, value)}>
               <Image
@@ -71,7 +110,12 @@ export const EmployeePageHeader = ({
             </button>
           </div>
         </div>
-        <EmployeeInfoPopUpButton title="직원 추가하기" />
+        <EmployeeInfoPopUpButton
+          title="직원 추가하기"
+          submitButtonName="추가하기"
+          type="add"
+          onSubmit={onSubmit}
+        />
       </div>
     </PageHeader>
   );
